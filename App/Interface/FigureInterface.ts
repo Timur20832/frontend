ef52e52b55530b821ed079a9307387a290668522
supperik <mr.niki234@mail.ru>
2023-09-29 13:30:32 +0300

    Доделал интерфейсы

--- App/Interface/FigureInterface.ts
+++ App/Interface/FigureInterface.ts
@@ -1,4 +1,19 @@
-interface SlideElement{
+interface Figure{
+    setId(id: number): void;
+    setPosX(x: number): void;
+    setPosY(y: number): void;
+    setSize(size: number): void;
+    setBorderColor(color: string): void;
+    select(): void;
+    unselect(): void;
+
+    getId(): number;
+    getPosX(): number;
+    getPosY(): number;
+    getSize(): number;
+    getBorderColor(): string;
+    getSelectCondition(): boolean;
+    
     setShape(shape: Form): void;
     setInnerColor(color: string): void;
     setWidth(width: number): void;
