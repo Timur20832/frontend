8f880a7c6a8bab7bfd3f69da61ffc34ade622872
supperik <63904974+supperik@users.noreply.github.com>
2023-11-12 21:15:02 +0300

    1111

--- src/App/blocks_view/SlideBox.js
+++ src/App/blocks_view/SlideBox.js
@@ -3,16 +3,16 @@ import { useState } from 'react';
 import { Slide } from '../types/types';
 
 export default function SlideBox() {
-    const [slideData, setSlideData] = useState();
-    return (
-      <>
+  const [slideData, setSlideData] = useState();
+  return (
+    <>
       <div style={{ backgroundColor: `#${Slide.backgroundColor}` }}>
-      <h1>Slide ID: {Slide.id}</h1><p>Number of Slide: {Slide.numberOfSlide}</p>
+      <h1>Slide ID: {Slide.id}</h1>
+      <p>Number of Slide: {Slide.numberOfSlide}</p>
       <p>Background Color: #{Slide.backgroundColor}</p>
       {Slide.elements.map((element, index) => (
-        <div key={index}>{element}</div>
+      <div key={index}>{element}</div>
       ))}
-
       </div>
     </>
   );
